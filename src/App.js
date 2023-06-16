e1ecb9c841d89e6b9fa1296e8a54912f30cf0d1f
KaleeswaraKumard <kaleeswarakumar1999@gmail.com>
2023-06-16 13:19:34 +0530

    * Updated the yaml workflow conflicts

--- src/App.js
+++ src/App.js
@@ -54,42 +54,42 @@ class myApp extends Component {
         <button style={{ color: "red", background: "black", border: "2px solid blue" }} onClick={() => console.log("clicked Me")}>
           Click Me
         </button>
-//         <div className="margin-left mt-3 mb-5" >
-//           <label htmlFor="Search" className="h5 mr-4">Search Country : </label>
-//           <input type="text" value={this.state.changeMe} onChange={this.showValue} />
-//         </div>
-//         <div className="container" >
-//           <div className="row border bg-dark text-white">
-//             <div className="col">
-//               <b className="h3">Country Name</b>
-//             </div>
-//             <div className="col">
-//               <b className="h3">Country Code </b>
-//             </div>
-//           </div>
+        <div className="margin-left mt-3 mb-5" >
+          <label htmlFor="Search" className="h5 mr-4">Search Country : </label>
+          <input type="text" value={this.state.changeMe} onChange={this.showValue} />
         </div>
-//         {
-//           this.state.valueChange.map((jsonValue, i) => {
-//             return (
-//               <div >
-//                 <div className="container text-dark border">
-//                   <div className="row">
-//                     <div className="col">
-//                       <span onClick={() => this.clickMe(jsonValue, i)}
-//                         className={this.state.clickedMe.includes(jsonValue) ?
-//                           "text-success pointer h6" : "text-primary pointer"}>
-//                         <b>{jsonValue.name}</b>
-//                       </span>
-//                     </div>
-//                     <div className="col">
-//                       <b>{jsonValue.code}</b>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </div>
-//             )
-//           })
-//         }
+        <div className="container" >
+          <div className="row border bg-dark text-white">
+            <div className="col">
+              <b className="h3">Country Name</b>
+            </div>
+            <div className="col">
+              <b className="h3">Country Code </b>
+            </div>
+          </div>
+        </div>
+        {/* {
+          this.state.valueChange.map((jsonValue, i) => {
+            return (
+              <div >
+                <div className="container text-dark border">
+                  <div className="row">
+                    <div className="col">
+                      <span onClick={() => this.clickMe(jsonValue, i)}
+                        className={this.state.clickedMe.includes(jsonValue) ?
+                          "text-success pointer h6" : "text-primary pointer"}>
+                        <b>{jsonValue.name}</b>
+                      </span>
+                    </div>
+                    <div className="col">
+                      <b>{jsonValue.code}</b>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        } */}
       </div>
     )
   }
